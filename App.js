e35919a7bae45731a0ea928b623b71c6ed8e0806
AnuradhaDev96 <anusampath9470@gmail.com>
2023-08-20 13:30:24 +0530

    App.js added Contact page added

--- App.js
+++ App.js
@@ -2,13 +2,28 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
+import Contact from './components/Contact';
 
 const App = () => {
   return (
     <Router>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container">
+          <Link className="navbar-brand" to="/">My Blog</Link>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">About</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
         {/* Add more routes here */}
       </Switch>
     </Router>
