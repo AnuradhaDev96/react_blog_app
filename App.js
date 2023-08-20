0ee6ffd2b0f34aac92e3e78d2ddae2653d25e057
AnuradhaDev96 <anusampath9470@gmail.com>
2023-08-20 14:59:15 +0530

    addded bootstrap line

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return (
